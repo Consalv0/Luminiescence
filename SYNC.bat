53e25fa93089c8e4e8bc257201c7a824d976829e
Gonzalo Ceciliano <gonzaloceciliano@gmail.com>
2020-10-22 02:22:15 -0500

    new bedrock variant, updated bedrock

---
+++ SYNC.bat
@@ -0,0 +1,4 @@
+xcopy ".\assets\" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\Luminiescence\assets\" /e /i /s /d /y
+xcopy ".\pack.*" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\Luminiescence\" /e /i /s /d /y
+xcopy ".\*.json" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\Luminiescence\" /e /i /s /d /y
+pause
