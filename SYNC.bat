5af448611e19105e0bf44a13bc0c478c7e54585d
Gonzalo Ceciliano <gonzalo.ceciliano@newfangled.games>
2024-01-27 12:38:54 +0000

    Improved sync script

--- SYNC.bat
+++ SYNC.bat
@@ -1,4 +1,7 @@
-xcopy ".\assets\" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\Luminiescence\assets\" /e /i /s /d /y
+:start
+:: xcopy ".\assets\" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\Luminiescence\assets\" /e /i /s /d /y
 xcopy ".\pack.*" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\Luminiescence\" /e /i /s /d /y
 xcopy ".\*.json" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks\Luminiescence\" /e /i /s /d /y
+robocopy .\assets\ %UserProfile%\AppData\Roaming\.minecraft\resourcepacks\Luminiescence\assets\ /mir
 pause
+goto:start
